97fcf547c2217d30c1c2a68cfec4606b666c368f
Vadim Shakun <vadim.shakun@gmail.com>
2016-11-18 00:56:48 +0300

    Append list of the available commands

---
+++ common/model.go
@@ -0,0 +1,31 @@
+package common
+
+
+type interface DataNull {
+    
+}
+
+type interface DataBool {
+
+}
+
+type interface DataInt {
+
+}
+
+type interface DataReal {
+
+}
+
+type interface DataString{
+
+} 
+
+type interface DataArray {
+
+}
+
+
+type interface DataDictionary {
+
+}
