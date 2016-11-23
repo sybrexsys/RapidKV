663200a73e64248c58731164fdb4e0e3e7aa57df
Vadim Shakun <vadim.shakun@gmail.com>
2016-11-23 13:22:31 +0300

    Update test

--- datamodel/model_test.go
+++ datamodel/model_test.go
@@ -321,52 +321,419 @@ func TestParsing(t *testing.T) {
 
 var longjson = `
 {
-    "_id" : "50ab0f8bbcf1bfe2536dc3f9",
-    "body" : "Amendment I\n<p>Congress shall make no law respecting an establishment of religion, or prohibiting the free exercise thereof; or abridging the freedom of speech, or of the press; or the right of the people peaceably to assemble, and to petition the Government for a redress of grievances.\n<p>\nAmendment II\n<p>\nA well regulated Militia, being necessary to the security of a free State, the right of the people to keep and bear Arms, shall not be infringed.\n<p>\nAmendment III\n<p>\nNo Soldier shall, in time of peace be quartered in any house, without the consent of the Owner, nor in time of war, but in a manner to be prescribed by law.\n<p>\nAmendment IV\n<p>\nThe right of the people to be secure in their persons, houses, papers, and effects, against unreasonable searches and seizures, shall not be violated, and no Warrants shall issue, but upon probable cause, supported by Oath or affirmation, and particularly describing the place to be searched, and the persons or things to be seized.\n<p>\nAmendment V\n<p>\nNo person shall be held to answer for a capital, or otherwise infamous crime, unless on a presentment or indictment of a Grand Jury, except in cases arising in the land or naval forces, or in the Militia, when in actual service in time of War or public danger; nor shall any person be subject for the same offence to be twice put in jeopardy of life or limb; nor shall be compelled in any criminal case to be a witness against himself, nor be deprived of life, liberty, or property, without due process of law; nor shall private property be taken for public use, without just compensation.\n<p>\n\nAmendment VI\n<p>\nIn all criminal prosecutions, the accused shall enjoy the right to a speedy and public trial, by an impartial jury of the State and district wherein the crime shall have been committed, which district shall have been previously ascertained by law, and to be informed of the nature and cause of the accusation; to be confronted with the witnesses against him; to have compulsory process for obtaining witnesses in his favor, and to have the Assistance of Counsel for his defence.\n<p>\nAmendment VII\n<p>\nIn Suits at common law, where the value in controversy shall exceed twenty dollars, the right of trial by jury shall be preserved, and no fact tried by a jury, shall be otherwise re-examined in any Court of the United States, than according to the rules of the common law.\n<p>\nAmendment VIII\n<p>\nExcessive bail shall not be required, nor excessive fines imposed, nor cruel and unusual punishments inflicted.\n<p>\nAmendment IX\n<p>\nThe enumeration in the Constitution, of certain rights, shall not be construed to deny or disparage others retained by the people.\n<p>\nAmendment X\n<p>\nThe powers not delegated to the United States by the Constitution, nor prohibited by it to the States, are reserved to the States respectively, or to the people.\"\n<p>\n",
-    "permalink" : "aRjNnLZkJkTyspAIoRGe",
-    "author" : "machine",
-    "title" : "Bill of Rights",
-    "tags" : [ 
-        "watchmaker", 
-        "santa", 
-        "xylophone", 
-        "math", 
-        "handsaw", 
-        "dream", 
-        "undershirt", 
-        "dolphin", 
-        "tanker", 
-        "action",
-		1234355534,
-		-122323,
-		true,
-		false,
-		null,
-		1454564.334343233
-    ],
-    "comments" : [ 
-        {
-            "body" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in ",
-            "email" : "HvizfYVx@pKvLaagH.com",
-            "author" : "Santiago Dollins"
-        }, 
-        {
-            "body" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in ",
-            "email" : "WpOUCpdD@hccdxJvT.com",
-            "author" : "Jaclyn Morado"
-        }, 
-        {
-            "body" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in ",
-            "email" : "OgDzHfFN@cWsDtCtx.com",
-            "author" : "Houston Valenti"
-        }, 
-        {
-            "body" : "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in ",
-            "email" : "tMNoFYPb@lymMpAyS.com",
-            "author" : "Whitley Fears"
-        } 
-     ],
-    "date" : "2012-11-20T05:05:15.231Z"
+  "statuses": [
+    {
+      "coordinates": null,
+      "favorited": false,
+      "truncated": false,
+      "created_at": "Mon Sep 24 03:35:21 +0000 2012",
+      "id_str": "250075927172759552",
+      "entities": {
+        "urls": [
+ 
+        ],
+        "hashtags": [
+          {
+            "text": "freebandnames",
+            "indices": [
+              20,
+              34
+            ]
+          }
+        ],
+        "user_mentions": [
+ 
+        ]
+      },
+      "in_reply_to_user_id_str": null,
+      "contributors": null,
+      "text": "Aggressive Ponytail #freebandnames",
+      "metadata": {
+        "iso_language_code": "en",
+        "result_type": "recent"
+      },
+      "retweet_count": 0,
+      "in_reply_to_status_id_str": null,
+      "id": 250075927172759552,
+      "geo": null,
+      "retweeted": false,
+      "in_reply_to_user_id": null,
+      "place": null,
+      "user": {
+        "profile_sidebar_fill_color": "DDEEF6",
+        "profile_sidebar_border_color": "C0DEED",
+        "profile_background_tile": false,
+        "name": "Sean Cummings",
+        "profile_image_url": "http://a0.twimg.com/profile_images/2359746665/1v6zfgqo8g0d3mk7ii5s_normal.jpeg",
+        "created_at": "Mon Apr 26 06:01:55 +0000 2010",
+        "location": "LA, CA",
+        "follow_request_sent": null,
+        "profile_link_color": "0084B4",
+        "is_translator": false,
+        "id_str": "137238150",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "expanded_url": null,
+                "url": "",
+                "indices": [
+                  0,
+                  0
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+ 
+            ]
+          }
+        },
+        "default_profile": true,
+        "contributors_enabled": false,
+        "favourites_count": 0,
+        "url": null,
+        "profile_image_url_https": "https://si0.twimg.com/profile_images/2359746665/1v6zfgqo8g0d3mk7ii5s_normal.jpeg",
+        "utc_offset": -28800,
+        "id": 137238150,
+        "profile_use_background_image": true,
+        "listed_count": 2,
+        "profile_text_color": "333333",
+        "lang": "en",
+        "followers_count": 70,
+        "protected": false,
+        "notifications": null,
+        "profile_background_image_url_https": "https://si0.twimg.com/images/themes/theme1/bg.png",
+        "profile_background_color": "C0DEED",
+        "verified": false,
+        "geo_enabled": true,
+        "time_zone": "Pacific Time (US & Canada)",
+        "description": "Born 330 Live 310",
+        "default_profile_image": false,
+        "profile_background_image_url": "http://a0.twimg.com/images/themes/theme1/bg.png",
+        "statuses_count": 579,
+        "friends_count": 110,
+        "following": null,
+        "show_all_inline_media": false,
+        "screen_name": "sean_cummings"
+      },
+      "in_reply_to_screen_name": null,
+      "source": "<a href=\"//itunes.apple.com/us/app/twitter/id409789998?mt=12%5C%22\" rel=\"\\\"nofollow\\\"\">Twitter for Mac</a>",
+      "in_reply_to_status_id": null
+    },
+    {
+      "coordinates": null,
+      "favorited": false,
+      "truncated": false,
+      "created_at": "Fri Sep 21 23:40:54 +0000 2012",
+      "id_str": "249292149810667520",
+      "entities": {
+        "urls": [
+ 
+        ],
+        "hashtags": [
+          {
+            "text": "FreeBandNames",
+            "indices": [
+              20,
+              34
+            ]
+          }
+        ],
+        "user_mentions": [
+ 
+        ]
+      },
+      "in_reply_to_user_id_str": null,
+      "contributors": null,
+      "text": "Thee Namaste Nerdz. #FreeBandNames",
+      "metadata": {
+        "iso_language_code": "pl",
+        "result_type": "recent"
+      },
+      "retweet_count": 0,
+      "in_reply_to_status_id_str": null,
+      "id": 249292149810667520,
+      "geo": null,
+      "retweeted": false,
+      "in_reply_to_user_id": null,
+      "place": null,
+      "user": {
+        "profile_sidebar_fill_color": "DDFFCC",
+        "profile_sidebar_border_color": "BDDCAD",
+        "profile_background_tile": true,
+        "name": "Chaz Martenstein",
+        "profile_image_url": "http://a0.twimg.com/profile_images/447958234/Lichtenstein_normal.jpg",
+        "created_at": "Tue Apr 07 19:05:07 +0000 2009",
+        "location": "Durham, NC",
+        "follow_request_sent": null,
+        "profile_link_color": "0084B4",
+        "is_translator": false,
+        "id_str": "29516238",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "expanded_url": null,
+                "url": "http://bullcityrecords.com/wnng/",
+                "indices": [
+                  0,
+                  32
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+ 
+            ]
+          }
+        },
+        "default_profile": false,
+        "contributors_enabled": false,
+        "favourites_count": 8,
+        "url": "http://bullcityrecords.com/wnng/",
+        "profile_image_url_https": "https://si0.twimg.com/profile_images/447958234/Lichtenstein_normal.jpg",
+        "utc_offset": -18000,
+        "id": 29516238,
+        "profile_use_background_image": true,
+        "listed_count": 118,
+        "profile_text_color": "333333",
+        "lang": "en",
+        "followers_count": 2052,
+        "protected": false,
+        "notifications": null,
+        "profile_background_image_url_https": "https://si0.twimg.com/profile_background_images/9423277/background_tile.bmp",
+        "profile_background_color": "9AE4E8",
+        "verified": false,
+        "geo_enabled": false,
+        "time_zone": "Eastern Time (US & Canada)",
+        "description": "You will come to Durham, North Carolina. I will sell you some records then, here in Durham, North Carolina. Fun will happen.",
+        "default_profile_image": false,
+        "profile_background_image_url": "http://a0.twimg.com/profile_background_images/9423277/background_tile.bmp",
+        "statuses_count": 7579,
+        "friends_count": 348,
+        "following": null,
+        "show_all_inline_media": true,
+        "screen_name": "bullcityrecords"
+      },
+      "in_reply_to_screen_name": null,
+      "source": "web",
+      "in_reply_to_status_id": null
+    },
+    {
+      "coordinates": null,
+      "favorited": false,
+      "truncated": false,
+      "created_at": "Fri Sep 21 23:30:20 +0000 2012",
+      "id_str": "249289491129438208",
+      "entities": {
+        "urls": [
+ 
+        ],
+        "hashtags": [
+          {
+            "text": "freebandnames",
+            "indices": [
+              29,
+              43
+            ]
+          }
+        ],
+        "user_mentions": [
+ 
+        ]
+      },
+      "in_reply_to_user_id_str": null,
+      "contributors": null,
+      "text": "Mexican Heaven, Mexican Hell #freebandnames",
+      "metadata": {
+        "iso_language_code": "en",
+        "result_type": "recent"
+      },
+      "retweet_count": 0,
+      "in_reply_to_status_id_str": null,
+      "id": 249289491129438208,
+      "geo": null,
+      "retweeted": false,
+      "in_reply_to_user_id": null,
+      "place": null,
+      "user": {
+        "profile_sidebar_fill_color": "99CC33",
+        "profile_sidebar_border_color": "829D5E",
+        "profile_background_tile": false,
+        "name": "Thomas John Wakeman",
+        "profile_image_url": "http://a0.twimg.com/profile_images/2219333930/Froggystyle_normal.png",
+        "created_at": "Tue Sep 01 21:21:35 +0000 2009",
+        "location": "Kingston New York",
+        "follow_request_sent": null,
+        "profile_link_color": "D02B55",
+        "is_translator": false,
+        "id_str": "70789458",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "expanded_url": null,
+                "url": "",
+                "indices": [
+                  0,
+                  0
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+ 
+            ]
+          }
+        },
+        "default_profile": false,
+        "contributors_enabled": false,
+        "favourites_count": 19,
+        "url": null,
+        "profile_image_url_https": "https://si0.twimg.com/profile_images/2219333930/Froggystyle_normal.png",
+        "utc_offset": -18000,
+        "id": 70789458,
+        "profile_use_background_image": true,
+        "listed_count": 1,
+        "profile_text_color": "3E4415",
+        "lang": "en",
+        "followers_count": 63,
+        "protected": false,
+        "notifications": null,
+        "profile_background_image_url_https": "https://si0.twimg.com/images/themes/theme5/bg.gif",
+        "profile_background_color": "352726",
+        "verified": false,
+        "geo_enabled": false,
+        "time_zone": "Eastern Time (US & Canada)",
+        "description": "Science Fiction Writer, sort of. Likes Superheroes, Mole People, Alt. Timelines.",
+        "default_profile_image": false,
+        "profile_background_image_url": "http://a0.twimg.com/images/themes/theme5/bg.gif",
+        "statuses_count": 1048,
+        "friends_count": 63,
+        "following": null,
+        "show_all_inline_media": false,
+        "screen_name": "MonkiesFist"
+      },
+      "in_reply_to_screen_name": null,
+      "source": "web",
+      "in_reply_to_status_id": null
+    },
+    {
+      "coordinates": null,
+      "favorited": false,
+      "truncated": false,
+      "created_at": "Fri Sep 21 22:51:18 +0000 2012",
+      "id_str": "249279667666817024",
+      "entities": {
+        "urls": [
+ 
+        ],
+        "hashtags": [
+          {
+            "text": "freebandnames",
+            "indices": [
+              20,
+              34
+            ]
+          }
+        ],
+        "user_mentions": [
+ 
+        ]
+      },
+      "in_reply_to_user_id_str": null,
+      "contributors": null,
+      "text": "The Foolish Mortals #freebandnames",
+      "metadata": {
+        "iso_language_code": "en",
+        "result_type": "recent"
+      },
+      "retweet_count": 0,
+      "in_reply_to_status_id_str": null,
+      "id": 249279667666817024,
+      "geo": null,
+      "retweeted": false,
+      "in_reply_to_user_id": null,
+      "place": null,
+      "user": {
+        "profile_sidebar_fill_color": "BFAC83",
+        "profile_sidebar_border_color": "615A44",
+        "profile_background_tile": true,
+        "name": "Marty Elmer",
+        "profile_image_url": "http://a0.twimg.com/profile_images/1629790393/shrinker_2000_trans_normal.png",
+        "created_at": "Mon May 04 00:05:00 +0000 2009",
+        "location": "Wisconsin, USA",
+        "follow_request_sent": null,
+        "profile_link_color": "3B2A26",
+        "is_translator": false,
+        "id_str": "37539828",
+        "entities": {
+          "url": {
+            "urls": [
+              {
+                "expanded_url": null,
+                "url": "http://www.omnitarian.me",
+                "indices": [
+                  0,
+                  24
+                ]
+              }
+            ]
+          },
+          "description": {
+            "urls": [
+ 
+            ]
+          }
+        },
+        "default_profile": false,
+        "contributors_enabled": false,
+        "favourites_count": 647,
+        "url": "http://www.omnitarian.me",
+        "profile_image_url_https": "https://si0.twimg.com/profile_images/1629790393/shrinker_2000_trans_normal.png",
+        "utc_offset": -21600,
+        "id": 37539828,
+        "profile_use_background_image": true,
+        "listed_count": 52,
+        "profile_text_color": "000000",
+        "lang": "en",
+        "followers_count": 608,
+        "protected": false,
+        "notifications": null,
+        "profile_background_image_url_https": "https://si0.twimg.com/profile_background_images/106455659/rect6056-9.png",
+        "profile_background_color": "EEE3C4",
+        "verified": false,
+        "geo_enabled": false,
+        "time_zone": "Central Time (US & Canada)",
+        "description": "Cartoonist, Illustrator, and T-Shirt connoisseur",
+        "default_profile_image": false,
+        "profile_background_image_url": "http://a0.twimg.com/profile_background_images/106455659/rect6056-9.png",
+        "statuses_count": 3575,
+        "friends_count": 249,
+        "following": null,
+        "show_all_inline_media": true,
+        "screen_name": "Omnitarian"
+      },
+      "in_reply_to_screen_name": null,
+      "source": "<a href=\"//twitter.com/download/iphone%5C%22\" rel=\"\\\"nofollow\\\"\">Twitter for iPhone</a>",
+      "in_reply_to_status_id": null
+    }
+  ],
+  "search_metadata": {
+    "max_id": 250126199840518145,
+    "since_id": 24012619984051000,
+    "refresh_url": "?since_id=250126199840518145&q=%23freebandnames&result_type=mixed&include_entities=1",
+    "next_results": "?max_id=249279667666817023&q=%23freebandnames&count=4&include_entities=1&result_type=mixed",
+    "count": 4,
+    "completed_in": 0.035,
+    "since_id_str": "24012619984051000",
+    "query": "%23freebandnames",
+    "max_id_str": "250126199840518145"
+  }
 }
 
 `
@@ -416,7 +783,7 @@ func BenchmarkSaveJsonOneAlloc(b *testing.B) {
 			b.Fatal("Error " + err.Error())
 		}
 	}
-	b.Log("Total processed bytes:", b.N*len(tot), " ", time.Since(t))
+	b.Log("Total processed bytes:", b.N*l, " ", time.Since(t))
 }
 
 func BenchmarkSaveJsonEachAlloc(b *testing.B) {
@@ -428,13 +795,15 @@ func BenchmarkSaveJsonEachAlloc(b *testing.B) {
 	if err != nil {
 		b.Fatal("Error " + err.Error())
 	}
+	ll := 0
 	for n := 0; n < b.N; n++ {
 		l := obj.getLength()
+		ll += l
 		m := make([]byte, l)
 		_, err = obj.writeToBytes(m)
 		if err != nil {
 			b.Fatal("Error " + err.Error())
 		}
 	}
-	b.Log("Total processed bytes:", b.N*len(tot), " ", time.Since(t))
+	b.Log("Total processed bytes:", ll, " ", time.Since(t))
 }
